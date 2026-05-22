45ab05e4b0bde3827167679ec8542ddcff5e5c48
skps00 <skps952608@gmail.com>
2026-05-22 14:29:29 +0800

    chore: remove osmdroid dependency and config

--- app/src/main/java/com/earthonline/app/EarthOnlineApplication.kt
+++ app/src/main/java/com/earthonline/app/EarthOnlineApplication.kt
@@ -2,16 +2,6 @@ package com.earthonline.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import org.osmdroid.config.Configuration
 
 @HiltAndroidApp
-class EarthOnlineApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Configuration.getInstance().apply {
-            userAgentValue = packageName
-            osmdroidBasePath = cacheDir
-            osmdroidTileCache = cacheDir
-        }
-    }
-}
+class EarthOnlineApplication : Application()
