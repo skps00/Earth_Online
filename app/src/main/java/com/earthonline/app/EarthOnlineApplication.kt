832bcb7746573f5ca9928539bd3c9c2ed605e708
skps00 <skps952608@gmail.com>
2026-05-23 15:32:49 +0800

    debug: play sound on app startup to test SoundPool

--- app/src/main/java/com/earthonline/app/EarthOnlineApplication.kt
+++ app/src/main/java/com/earthonline/app/EarthOnlineApplication.kt
@@ -1,7 +1,19 @@
 package com.earthonline.app
 
 import android.app.Application
+import android.media.SoundPool
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class EarthOnlineApplication : Application()
+class EarthOnlineApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        try {
+            val sp = SoundPool.Builder().setMaxStreams(1).build()
+            val id = sp.load(this, R.raw.achievement_unlock, 1)
+            sp.setOnLoadCompleteListener { _, _, status ->
+                if (status == 0) sp.play(id, 1f, 1f, 1, 0, 1f)
+            }
+        } catch (_: Exception) { }
+    }
+}
